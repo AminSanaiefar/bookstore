c0b12cf4d8ab9aa628d543320cdc368a7fb40298
Amin Saniefar <aminsanaiefar@gmail.com.>
2024-12-04 12:51:00 +0330

    make tests for accounts &  pages and fix home_page url address

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_home_page_by_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_by_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    # test if in showed template Home Page Is showing --->
+    def test_home_page_content(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Home Page')
+
+    # test if the correct template is using for home page
+    def test_home_page_template_used(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
