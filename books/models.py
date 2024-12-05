d3e006a6adb8a31a422adb13701529fc262609b7
Amin Saniefar <aminsanaiefar@gmail.com.>
2024-12-05 22:32:28 +0330

    implement books app and create book model

---
+++ books/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+
+    def __str__(self):
+        return f'{self.author} - {self.title}'
