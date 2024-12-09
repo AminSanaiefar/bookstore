c2b7b210bfdf6e93efaf89d8440c13dc4b0abce5
Amin Saniefar <aminsanaiefar@gmail.com.>
2024-12-09 21:12:42 +0330

    add home pagination

--- pages/views.py
+++ pages/views.py
@@ -8,6 +8,7 @@ class HomeBookListView(generic.ListView):
     # model = Book
     template_name = 'home.html'
     context_object_name = 'books'
+    paginate_by = 6
 
     def get_queryset(self):
         return Book.objects.all().order_by('-id')
